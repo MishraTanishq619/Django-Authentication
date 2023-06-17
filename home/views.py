8a0186924b964a9d520c8614505b37a7c6205d3a
MishraTanishq619 <127047332+MishraTanishq619@users.noreply.github.com>
2023-06-18 00:01:00 +0530

    Add files via upload

---
+++ home/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render,redirect
+
+#
+from django.contrib.auth import authenticate , login
+from django.contrib.auth import logout
+
+# Create your views here.
+
+def index(request):
+    if request.user.is_anonymous:
+        return redirect('/login')
+    return render(request,'index.html')
+
+
+def loginuser(request):
+    if request.method=="POST":
+        uname=request.POST.get("uname")
+        password=request.POST.get("password")
+        # checking user creds
+        user = authenticate(username=uname,password=password)
+        if user is not None:
+            # A backend authenticated the credentials
+            login(request,user)
+            return redirect('/')
+        else:
+            # No backend authenticated the credentials
+            return render(request,'login.html')
+    
+    return render(request,'login.html')
+
+def logoutuser(request):
+    logout(request)
+    return render(request,'logout.html')
